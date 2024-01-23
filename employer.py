1fa2211ddc1f00e25da3f29a3701bdb313dabe5a
Dmitry Pavlushin <pavlushindv@gmail.com>
2024-01-23 20:12:53 +0300

    add employees

--- employer.py
+++ employer.py
@@ -5,4 +5,12 @@ import json
 class Employer:
 
     def employee_of_date(self):
-        pass
+        values = {'text': 'газ', 'area': 1, 'only_with_vacancies': True}
+
+        req = requests.get('https://api.hh.ru/employers', values)
+        data = req.json()
+
+        employees_file = 'employers.json'
+        with open(employees_file, 'w') as f:
+            json.dump(data, f, ensure_ascii=False)
+        return data
